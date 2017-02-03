4b8aa94a2078bae4072c75a5b90be7ca22eefb48
pixelgoo <pixelgoo@gmail.com>
2017-02-03 16:37:03 +0200

    Add new font and beta flag in logo

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module KlanzParser
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
   end
