7720ea8c260f99d91bf36eb477933a05e8758bb7
pixelgoo <pixelgoo@gmail.com>
2017-02-05 18:50:53 +0200

    Add my_decks route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   get '/about', to: 'static_pages#about'
+  get '/my_decks', to: 'decks#user_index'
   devise_for :users, controllers: { registrations: "users/registrations" }, :has_many => [:decks]
   resources :cards, param: :number, only: [:index, :show]
   resources :decks, except: [:edit, :update]
