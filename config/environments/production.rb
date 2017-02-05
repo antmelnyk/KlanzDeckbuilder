5d296859a49ffd2ab517224ec867c4a9b4bfd455
pixelgoo <pixelgoo@gmail.com>
2017-02-05 16:50:39 +0200

    Fix production mailing issues :two:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Devise mailer configuration
+  config.action_mailer.default_url_options = { :host => 'klanz-deckbuilder.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
