6a0b4100dd76d91ad79ac4591a6b4e70f2cfaa06
pixelgoo <pixelgoo@gmail.com>
2017-02-05 17:02:31 +0200

    Fix production mailing issues :three:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,10 @@ Rails.application.configure do
 
   # Devise mailer configuration
   config.action_mailer.default_url_options = { :host => 'klanz-deckbuilder.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
+  config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
       domain: "gmail.com",
