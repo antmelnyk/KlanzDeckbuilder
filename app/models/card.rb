640be8d1031c3bef5bffad1620f81f0758868f96
pixelgoo <pixelgoo@gmail.com>
2017-02-12 18:59:13 +0200

    Add basic abilities search

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,4 +7,34 @@ class Card < ApplicationRecord
   scope :power, -> power { where power: power }
   scope :damage, -> damage { where damage: damage}
 
+  # Handling search queries
+  MODIFIER = [/\A(эхо)|\A(гнев)|\A(крах)/]
+
+
+  def correct_query(search)
+    case search
+      when '+ атака'
+        /\+[0-9]* (атак)/
+      when '- атака'
+        /-[0-9]* (атак)/
+      when '+ сила'
+        /\+[0-9]* (сил)/
+      when '- сила'
+        /-[0-9]* (сил)/
+      when '+ жизнь'
+        /\+[0-9]* (жизн)/
+      when '- жизнь'
+        /-[0-9]* (сил)/
+
+      else
+        search
+    end
+  end
+
+  scope :search, -> search do
+    search = Card.correct_query(search)
+    where("ability ~* ?", "(first|last)\s+(last|first)")
+    where "ability ILIKE ? OR name ILIKE ?", "%#{search}%", "%#{search}%"
+  end
+
 end
