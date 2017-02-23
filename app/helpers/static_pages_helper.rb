28b0ef0f6b6cb408f8b94971ce17783c05cb7f7d
pixelgoo <pixelgoo@gmail.com>
2017-02-23 20:45:29 +0200

    Add Master and Margareth

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -4,7 +4,8 @@ module StaticPagesHelper
         1771 => Time.new(2017, 3, 25), # Крампус
         1779 => Time.new(2017, 2, 23), # Тандер
         1781 => Time.new(2017, 3, 1), # Нифф
-        1807 => Time.new(2017, 5, 10) # Илья
+        1807 => Time.new(2017, 5, 10), # Илья
+        1805 => Time.new(2017, 3, 17) # Мастер
     }
     date = startime[card.number].to_f * 1000
   end
