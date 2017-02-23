873a29205a201e1690c22b869c2398e52e9942d7
pixelgoo <pixelgoo@gmail.com>
2017-02-23 20:22:52 +0200

    Add Iliya

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,7 +3,8 @@ module StaticPagesHelper
     startime = {
         1771 => Time.new(2017, 3, 25), # Крампус
         1779 => Time.new(2017, 2, 23), # Тандер
-        1781 => Time.new(2017, 3, 1) # Нифф
+        1781 => Time.new(2017, 3, 1), # Нифф
+        1807 => Time.new(2017, 5, 10) # Илья
     }
     date = startime[card.number].to_f * 1000
   end
